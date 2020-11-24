00f65d009d0852ff3c84c22ac5b9dd4a398a29be
Sam Markham <sam.e.markham@gmail.com>
2020-11-24 14:50:38 -0800

    Install react-router-dom and add links to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      Test
-    </div>
+    <nav>
+      <Link className="nav-link" to="/">Home</Link>
+      <Link className="nav-link" to="/projects">Projects</Link>
+      <Link className="nav-link" to="/blog">Blog</Link>
+      <Link className="nav-link" to="/resume">Resume</Link>
+      <Link className="nav-link" to="/about">About</Link>
+      <Link className="nav-link" to="/contact">Contact</Link>
+    </nav>
   );
 }
 
