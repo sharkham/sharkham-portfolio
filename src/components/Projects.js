49cfcc5ec9ab5ecd9edd5e8fc2f0704bb24d1254
Sam Markham <sam.e.markham@gmail.com>
2020-11-24 16:44:03 -0800

    Add dummy project info and styling for project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,24 @@
+import '../App.css';
 import React from 'react';
 
 const Projects = () => {
   return (
     <div>
-      Projects
+      <div className="project-card">
+        <h2>Novel Tracker App</h2>
+        <p>A novel tracking app</p>
+        <p className="project-languages">React, Redux, Rails (API), ReactStrap</p>
+      </div>
+      <div className="project-card">
+        <h2>PokéTeamBuilder</h2>
+        <p>A Pokémon size comparison app</p>
+        <p className="project-languages">VanillaJS, Rails (API)</p>
+      </div>
+      <div className="project-card">
+        <h2>Prompt Generator</h2>
+        <p>A writing prompt generator</p>
+        <p className="project-languages">React (functional), CSS</p>
+      </div>
     </div>
   );
 }
