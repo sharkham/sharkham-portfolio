4801dedd4b52644700840177ca25955c6ab1ad94
Sam Markham <sam.e.markham@gmail.com>
2020-11-26 15:07:00 -0800

    Show more info for projects on click

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -3,18 +3,21 @@ export const projects = [
     name: "Novel Tracker App",
     description: "A novel tracking app",
     languages: "React, Redux, Rails (API), ReactStrap",
-    github: "https://github.com/sharkham/novel-tracker-frontend"
+    github: "https://github.com/sharkham/novel-tracker-frontend",
+    moreinfo: "More info test"
   },
   {
     name: "PokéTeamBuilder",
     description: "A Pokémon size comparison app",
     languages: "VanillaJS, Rails (API)",
-    github: "https://github.com/sharkham/PokeTeamBuilder"
+    github: "https://github.com/sharkham/PokeTeamBuilder",
+    moreinfo: "More info test"
   },
   {
     name: "Prompt Generator",
     description: "A writing prompt generator",
     languages: "React (functional), CSS",
-    github: "https://github.com/sharkham/prompt-generator"
+    github: "https://github.com/sharkham/prompt-generator",
+    moreinfo: "More info test"
   }
 ]
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Project = (props) => {
 
-  let { name, description, languages, github } = props.data;
+  const { name, description, languages, github, moreinfo } = props.data;
+
+  const [moreInfoVisibility, setMoreInfoVisibility] = useState(false);
+
+  const toggleMoreInfo = (e) => {
+    e.preventDefault()
+    setMoreInfoVisibility(!moreInfoVisibility)
+  }
+
+  const moreInfo = () => {
+    return (
+      <p>{moreinfo}</p>
+    )
+  }
+
   return (
     <div className="project-card">
       <h2>{name}</h2>
       <p>{description}</p>
       <p className="project-languages">{languages}</p>
+      <button onClick={(e) => toggleMoreInfo(e)}>More Info</button>
+      {(moreInfoVisibility) && moreInfo()}
     </div>
   );
 }
