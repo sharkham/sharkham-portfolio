e6e450f53e3bb1dbc06d233aa9f34d3ba2745816
Sam Markham <sam.e.markham@gmail.com>
2020-11-26 14:22:35 -0800

    Remove Novel Tracker component and make an array of objects of project data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import NovelTracker from './NovelTracker';
 
 const Projects = () => {
   return (
     <div>
-      <NovelTracker/>
-      <div className="project-card">
-        <h2>Novel Tracker App</h2>
-        <p>A novel tracking app</p>
-        <p className="project-languages">React, Redux, Rails (API), ReactStrap</p>
-      </div>
       <div className="project-card">
         <h2>PokéTeamBuilder</h2>
         <p>A Pokémon size comparison app</p>
         <p className="project-languages">VanillaJS, Rails (API)</p>
-      </div>
-      <div className="project-card">
-        <h2>Prompt Generator</h2>
-        <p>A writing prompt generator</p>
-        <p className="project-languages">React (functional), CSS</p>
       </div>
     </div>
   );
