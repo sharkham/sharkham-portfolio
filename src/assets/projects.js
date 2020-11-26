15cc03c730c3db4a63d9ded9c41bf31ec6baafe4
Sam Markham <sam.e.markham@gmail.com>
2020-11-26 14:22:50 -0800

    Make an array of objects of project data

---
+++ src/assets/projects.js
@@ -0,0 +1,20 @@
+export default projects = [
+  {
+    name: "Novel Tracker App",
+    description: "A novel tracking app",
+    languages: "React, Redux, Rails (API), ReactStrap",
+    github: "https://github.com/sharkham/novel-tracker-frontend"
+  },
+  {
+    name: "PokéTeamBuilder",
+    description: "A Pokémon size comparison app",
+    languages: "VanillaJS, Rails (API)",
+    github: "https://github.com/sharkham/PokeTeamBuilder"
+  },
+  {
+    name: "Prompt Generator",
+    description: "A writing prompt generator",
+    languages: "React (functional), CSS",
+    github: "https://github.com/sharkham/prompt-generator"
+  }
+]
