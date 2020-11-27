4a29b61c8ba5e826f7ef625cc2fe73e42c099622
Sam Markham <sam.e.markham@gmail.com>
2020-11-26 18:01:06 -0800

    Add LinkedIn blurb to About page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      About
+      <p>Full stack software engineer with experience in Ruby and Javascript, including Rails and React frameworks. Developing further skills in SQL and Postgres. My previous life includes a decade of experience in problem solving through writing, editing, critique and collaboration across many fields, from fiction and a published sociology paper to graphic novels and press releases. My blend of skills in collaboration, communication, analysis and creativity supplement my hard skills in coding. I enjoy putting my mind to a blank page and crafting something tangible and useful.</p>
     </div>
   );
 }
