14261aca70dcab8bdb60d3ec9adee063ef5fd5ac
Sam Markham <sam.e.markham@gmail.com>
2020-11-27 13:54:37 -0800

    Add resume and download link for it

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Resume = () => {
   return (
     <div>
-      Resume
+      <p>Download my resume <Link to="/Sam-Markham-resume.pdf" target="_blank" download>here</Link>!</p>
     </div>
   );
 }
