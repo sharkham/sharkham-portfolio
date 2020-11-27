db5c7150d58807470a5ea7db27f851c7183e5ffa
Sam Markham <sam.e.markham@gmail.com>
2020-11-26 17:03:40 -0800

    Add code to fetch blog posts from Dev profile

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Blog = () => {
+
+  const [hasError, setErrors] = useState(false);
+  const [blogPosts, setBlogPosts] = useState({});
+
+  async function fetchData() {
+    const res = await fetch("https://dev.to/api/articles?username=sharkham");
+    res
+      .json()
+      .then(res => setBlogPosts(res))
+      .catch(err => setErrors(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+  });
+
   return (
     <div>
-      Blog
+      <span>{JSON.stringify(blogPosts)}</span>
+      <hr />
+      <span>Has error: {JSON.stringify(hasError)}</span>
     </div>
   );
 }
