1843e4ed2f156b34201434761372b052f103fdfe
Sam Markham <sam.e.markham@gmail.com>
2020-11-27 15:44:48 -0800

    Add social links

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -8,11 +8,21 @@ import twitter from '../images/twitter-icon.png'
 const Socials = () => {
   return (
     <div className="social-icons-div">
-      <img src={dev} alt="DEV icon" className="social-icons" />
-      <img src={email} alt="Email icon" className="social-icons" />
-      <img src={github} alt="LinkedIn icon" className="social-icons" />
-      <img src={linkedin} alt="GitHub icon" className="social-icons" />
-      <img src={twitter} alt="Twitter icon" className="social-icons" />
+      <a href="https://dev.to/sharkham" target="_blank">
+        <img src={dev} alt="DEV icon" className="social-icons" />
+      </a>
+      <a href="mailto:sam.e.markham@gmail.com" target="_blank">
+        <img src={email} alt="Email icon" className="social-icons" />
+      </a>
+      <a href="https://github.com/sharkham" target="_blank">
+        <img src={github} alt="GitHub icon" className="social-icons" />
+      </a>
+      <a href="https://www.linkedin.com/in/markhamsam/" target="_blank">
+        <img src={linkedin} alt="LinkedIn icon" className="social-icons" />
+      </a>
+      <a href="https://twitter.com/sharkhamcodes" target="_blank">
+        <img src={twitter} alt="Twitter icon" className="social-icons" />
+      </a>
     </div>
   );
 }
