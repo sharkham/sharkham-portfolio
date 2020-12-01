929bc5e460177391cbf7be473f8e3f9d20d93072
Sam Markham <sam.e.markham@gmail.com>
2020-12-01 15:03:50 -0800

    Add more info to projects object

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -5,7 +5,31 @@ export const projects = [
     languages: "React, Redux, Rails (API), ReactStrap",
     github: "https://github.com/sharkham/novel-tracker-frontend",
     moreinfo: "More info test",
-    image: "noveltrackerappscreenshot.png"
+    image: "noveltrackerappscreenshot.png",
+    livedemo: "http://novel-tracker-app.netlify.app/",
+    demovideo: "https://youtu.be/e5lMdRyioSU",
+    relatedblogposts: [
+      {
+        title: "Dealing with overlapping reducers in Redux",
+        url: "https://dev.to/sharkham/dealing-with-overlapping-reducers-in-redux-dcm"
+      },
+      {
+        title: "Changing attributes on a user in Rails without having to re-enter password",
+        url: "https://dev.to/sharkham/changing-attributes-on-a-user-in-rails-without-having-to-re-enter-password-5bgb"
+      },
+      {
+        title: "Forgot/Reset Password functionality with Rails and React (and Redux)",
+        url: "https://dev.to/sharkham/forgot-reset-password-functionality-with-rails-and-react-and-redux-4kp0"
+      },
+      {
+        title: "Deploying a React (with Redux)/Rails app with Netlify/Heroku",
+        url: "https://dev.to/sharkham/deploying-a-react-with-redux-rails-app-with-netlify-heroku-5b89"
+      },
+      {
+        title: "Follow-up: deploying Forgot/Reset Password (React/Rails app)",
+        url: "https://dev.to/sharkham/follow-up-deploying-forgot-reset-password-react-rails-app-223j"
+      }
+    ]
   },
   {
     name: "PokéTeamBuilder",
@@ -13,7 +37,19 @@ export const projects = [
     languages: "VanillaJS, Rails (API)",
     github: "https://github.com/sharkham/PokeTeamBuilder",
     moreinfo: "More info test",
-    image: "../../public/noveltrackerapp.png"
+    image: "../../public/noveltrackerapp.png",
+    livedemo: null,
+    demovideo: "https://youtu.be/iJ1hqZwg1SM",
+    relatedblogposts: [
+      {
+        title: "Dragging images with mousemove in JavaScript",
+        url: "https://dev.to/sharkham/dragging-images-with-mousemove-in-javascript-2dn4"
+      },
+      {
+        title: "Vertical project planning for a Vanilla JavaScript/Rails app",
+        url: "https://dev.to/sharkham/vertical-project-planning-for-a-vanilla-javascript-rails-app-3k43"
+      }
+    ]
   },
   {
     name: "Prompt Generator",
@@ -21,6 +57,14 @@ export const projects = [
     languages: "React (functional), CSS",
     github: "https://github.com/sharkham/prompt-generator",
     moreinfo: "More info test",
-    image: "promptgeneratorscreenshot.png"
+    image: "promptgeneratorscreenshot.png",
+    livedemo: "https://promptgenerator.netlify.app/",
+    demovideo: null,
+    relatedblogposts: [
+      {
+        title: "Building a writing prompt generator with functional React",
+        url: "https://dev.to/sharkham/building-a-writing-prompt-generator-with-functional-react-1k1o"
+      }
+    ]
   }
 ]
