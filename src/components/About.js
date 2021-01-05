69e1152fc1c9aaad57f9e71a386096acbc00f92a
Sam Markham <sam.e.markham@gmail.com>
2021-01-05 15:33:20 -0800

    Add photo to about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import avatar from '../images/professional_avatar_2015 small.jpeg'
 
 const About = () => {
   return (
     <div>
+      <img id="avatar" src={avatar}/>
       <p>Full stack software engineer with experience in Ruby and Javascript, including Rails and React frameworks. Developing further skills in SQL and Postgres. My previous life includes a decade of experience in problem solving through writing, editing, critique and collaboration across many fields, from fiction and a published sociology paper to graphic novels and press releases. My blend of skills in collaboration, communication, analysis and creativity supplement my hard skills in coding. I enjoy putting my mind to a blank page and crafting something tangible and useful.</p>
     </div>
   );
