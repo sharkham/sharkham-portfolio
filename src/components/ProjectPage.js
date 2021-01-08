04d7643c7b1daa0a99610d38bd462f7dbc0db7c1
Sam Markham <sam.e.markham@gmail.com>
2021-01-08 15:05:58 -0800

    Add the logic for project info to display on ProjectPage if it exists

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -2,21 +2,57 @@ import React from 'react';
 import { projects } from '../assets/projects';
 
 const ProjectPage = ({ match }) => {
-  const matchUrls = () => {
+  let project = null;
+
+  const displayProjectInfo = () => {
+    // if (project !== null) {
+      console.log(project.name)
+      return (
+        <div>{ project.name }</div>
+      )
+    // } else {
+    //   return <p>Invalid URL</p>
+    // }
+  }
+
+  const nothingHere = () => {
+    console.log("nothinghere")
+    return <div>Invalid URL</div>
+  }
+
+  const validUrl = () => {
     //if the URL typed in matches one of the projects:
     //load that page
     //else:
     //display an error
-    const projectUrls = projects.map(project => {return project.url})
-    if (!projectUrls.includes(match.params.url)) {
-      console.log("not here")
-      return false
+    // const projectUrls = projects.map(p => {return p.url})
+    // if (!projectUrls.includes(match.params.url)) {
+    //   console.log("not here")
+    //   return false
+    // }
+
+    projects.forEach(p => {
+      if (p.url === match.params.url) {
+        project = p
+        console.log(p)
+      //   return true
+      //   // return displayProjectInfo()
+      //   // return p.title
+      // } else {
+      //   console.log("not here")
+      //   return false
+      }
+    })
+    if (project !== null) {
+      console.log("true")
+      return true
     }
   }
 
+
   return (
     <div>
-      { matchUrls() }
+      { validUrl() ? displayProjectInfo() : nothingHere() }
     </div>
   );
 }
