15748d35f1b3bb5c890845aafecb8d5d60bcf0e1
Sam Markham <sam.e.markham@gmail.com>
2021-01-08 17:03:01 -0800

    Add em-dash to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div>
       <p>Looking for a developer? I'm looking for remote or Vancouver-based work opportunities and would love to hear from you!</p>
-      <p>Want to chat about cats, Pokémon, or what makes a good critique? I love connecting with other devs--feel free to drop me a line!</p>
+      <p>Want to chat about cats, Pokémon, or what makes a good critique? I love connecting with other devs—feel free to drop me a line!</p>
     </div>
   );
 }
