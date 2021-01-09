deb7e3233e8af0b61ce3276ddd2a79fff760e0d7
Sam Markham <sam.e.markham@gmail.com>
2021-01-08 17:04:19 -0800

    Add links to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-      <p>Looking for a developer? I'm looking for remote or Vancouver-based work opportunities and would love to hear from you!</p>
-      <p>Want to chat about cats, Pokémon, or what makes a good critique? I love connecting with other devs—feel free to drop me a line!</p>
+      <p>Looking for a developer? I'm looking for remote or Vancouver-based work opportunities and would love to <a href="mailto:sam.e.markham@gmail.com" target="_blank" rel="noreferrer">hear from you</a>!</p>
+      <p>Want to chat about cats, Pokémon, or what makes a good critique? I love connecting with other devs—feel free to <a href="mailto:sam.e.markham@gmail.com" target="_blank" rel="noreferrer">drop me a line</a>!</p>
     </div>
   );
 }
