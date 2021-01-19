bac2c8588792b93eda286179dca52761dd78845e
Sam Markham <sam.e.markham@gmail.com>
2021-01-19 15:10:16 -0800

    Clean up Project card code

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,14 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Project = (props) => {
 
-  const { name, url, description, languages, github, image } = props.data;
+  const { name, url, description, languages, github } = props.data;
 
   return (
     <div className="project-card">
       <h2><Link to={`/projects/${url}`}>{name}</Link></h2>
       <p>{description}</p>
-      {/* <img src={process.env.PUBLIC_URL + image} alt='Screenshot of app'/> */}
-      {/* <br/> */}
       <p className="project-languages">{languages}</p>
     </div>
   );
