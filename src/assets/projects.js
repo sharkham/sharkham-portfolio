5273c69a18318e4184491d3b7322ad5c2ea4c529
Sam Markham <sam.e.markham@gmail.com>
2021-01-19 14:33:57 -0800

    Add another project and add more info text to other projects

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     description: "A novel tracking app",
     languages: "React, Redux, Rails (API), ReactStrap",
     github: "https://github.com/sharkham/novel-tracker-frontend",
-    moreinfo: "More info test",
+    moreinfo: "Ever wanted to run your own novel writing contest, or just keep track of your novel writing progress? Novel Tracker is a single page React app (with a Rails API as a back-end) where users can create a novel project for each year of a novel writing contest, edit it, update their wordcount (which displays in a progress bar) and earn badges for achievements. Users can also view basic information about other contest participants like novel title, summary, progress and badges.",
     image: "noveltrackerappscreenshot.png",
     livedemo: "http://novel-tracker-app.netlify.app/",
     demovideo: "https://youtu.be/e5lMdRyioSU",
@@ -38,7 +38,7 @@ export const projects = [
     description: "A Pokémon size comparison app",
     languages: "VanillaJS, Rails (API)",
     github: "https://github.com/sharkham/PokeTeamBuilder",
-    moreinfo: "More info test",
+    moreinfo: "Ever wanted an easy way to tell how big your favourite team of Pokémon is compared to you? PokéTeamBuilder is a single page JavaScript application (with a Rails API as a back-end) where users can create a trainer and select up to 6 Pokémon from generations 1-3 that will appear (mostly) sized according to their Pokédex height. Pokémon and trainers can then be dragged around on a background to create a team picture. Pokémon, trainer sprites, and positions are saved, and reloaded upon login.",
     image: "../../public/noveltrackerapp.png",
     livedemo: null,
     demovideo: "https://youtu.be/iJ1hqZwg1SM",
@@ -59,7 +59,7 @@ export const projects = [
     description: "A writing prompt generator",
     languages: "React (functional), CSS",
     github: "https://github.com/sharkham/prompt-generator",
-    moreinfo: "More info test More info testMore info testMore info testMore info testMore info testMore info testMore info testMore info testMore info testMore info testMore info testMore info test",
+    moreinfo: "Stuck on what to write? Try this simple writing prompt generator! Prompt Generator is a single page React app where users can generate writing prompts from multiple lists of words (currently a general set of random words or a set of nautical terms).",
     image: "promptgeneratorscreenshot.png",
     livedemo: "https://promptgenerator.netlify.app/",
     demovideo: null,
@@ -67,6 +67,22 @@ export const projects = [
       {
         title: "Building a writing prompt generator with functional React",
         url: "https://dev.to/sharkham/building-a-writing-prompt-generator-with-functional-react-1k1o"
+      }
+    ]
+  },
+  {
+    name: "Collab Writing App",
+    url: "collab-writing-app",
+    description: "A group novel writing app",
+    languages: "Rails, Google OAuth",
+    github: "https://github.com/sharkham/CollabWritingApp",
+    moreinfo: "Collab Writing App is a Rails(6)-based web application intended for use by writers working on group novel projects together. Within the app, a user can create a novel, only visible to them, and invite other users to the novel project to allow them to edit and add to it.",
+    image: "promptgeneratorscreenshot.jpg",
+    demovideo: "https://youtu.be/PoT4jxDS6rA",
+    relatedblogposts: [
+      {
+        title: "Creating two resources 'at once' in Rails",
+        url: "https://dev.to/sharkham/creating-two-resources-at-once-in-rails-386c"
       }
     ]
   }
