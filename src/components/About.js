5b547e8b68862d188ab3d4b61e8fab4f146247f1
Sam Markham <sam.e.markham@gmail.com>
2021-01-19 16:36:32 -0800

    Add alt text to image on about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import avatar from '../images/professional_avatar_2015 small.jpeg'
 const About = () => {
   return (
     <div>
-      <img id="avatar" src={avatar}/>
+      <img id="avatar" src={avatar} alt="Sam, a smiling white person with short blue hair"/>
       <p>Full stack software engineer with experience in Ruby and Javascript, including Rails and React frameworks. Developing further skills in SQL and Postgres. </p>
       <p>My previous life includes a decade of experience in problem solving through writing, editing, critique and collaboration across many fields, from fiction and a published sociology paper to graphic novels and press releases.</p>
       <p>My blend of skills in collaboration, communication, analysis and creativity supplement my hard skills in coding. I enjoy putting my mind to a blank page and crafting something tangible and useful.</p>
