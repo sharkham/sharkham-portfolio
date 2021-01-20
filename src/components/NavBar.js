49e0b7a6763985fed6a44d3469282898a7dc91b0
Sam Markham <sam.e.markham@gmail.com>
2021-01-20 14:16:12 -0800

    Remove Home link from navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <nav>
-      <Link className="nav-link" to="/">Home</Link>
+      {/* <Link className="nav-link" to="/">Home</Link> */}
       <Link className="nav-link" to="/projects">Projects</Link>
       <Link className="nav-link" to="/blog">Blog</Link>
       <Link className="nav-link" to="/about">About</Link>
