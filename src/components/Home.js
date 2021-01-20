6800bbfcde24d461c0f2b6b83186a2298aeb0c1d
Sam Markham <sam.e.markham@gmail.com>
2021-01-20 14:17:50 -0800

    Change h3 to h2 on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import CatPictureGenerator from './CatPictureGenerator';
 const Home = () => {
   return (
     <div>
-      <h3>Hi! I'm Sam, software engineer and writer.</h3>
+      <h2>Hi! I'm Sam, software engineer and writer.</h2>
       <p>This site was built with React and custom CSS. Click around to see more about me and my work, or enjoy some pictures of my cat here!</p>
       <CatPictureGenerator/>
     </div>
