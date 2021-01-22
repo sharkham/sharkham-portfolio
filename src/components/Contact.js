83c27862018be5938166ccd42d983942df317e06
Sam Markham <sam.e.markham@gmail.com>
2021-01-22 15:41:26 -0800

    Add email icon to contact page and remove links to email in text on that page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import email from '../images/email-icon.png'
 
 const Contact = () => {
   return (
     <div>
-      <p>Looking for a developer? I'm looking for remote or Vancouver-based work opportunities and would love to <a href="mailto:sam.e.markham@gmail.com" target="_blank" rel="noreferrer">hear from you</a>!</p>
-      <p>Want to chat about cats, Pokémon, or what makes a good critique? I love connecting with other devs—feel free to <a href="mailto:sam.e.markham@gmail.com" target="_blank" rel="noreferrer">drop me a line</a>!</p>
+      <p>Looking for a developer? I'm looking for remote or Vancouver-based work opportunities and would love to hear from you!</p>
+      <p>Want to chat about cats, Pokémon, or what makes a good critique? I love connecting with other devs—feel free to drop me a line!</p>
+      <div className="contact-icons-div">
+        <a href="mailto:sam.e.markham@gmail.com" target="_blank" rel="noreferrer">
+          <img src={email} alt="Email icon" className="contact-icons" />
+        </a>
+      </div>
     </div>
   );
 }
