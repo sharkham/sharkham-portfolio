6c3706e654ecb925639f1a750368e5d6cfd8a2dc
Sam Markham <sam.e.markham@gmail.com>
2021-01-26 20:18:08 -0800

    Add | between different nav links and adjust spacing to make it fit on desktop and mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,12 @@ const NavBar = () => {
   return (
     <nav>
       {/* <Link className="nav-link" to="/">Home</Link> */}
-      <Link className="nav-link" to="/projects">Projects</Link>
+      <Link className="nav-link nav-link-first" to="/projects">Projects</Link>
+      <div className="link-divider">|</div>
       <Link className="nav-link" to="/blog">Blog</Link>
+      <div className="link-divider">|</div>
       <Link className="nav-link" to="/about">About</Link>
+      <div className="link-divider">|</div>
       <Link className="nav-link" to="/contact">Contact</Link>
     </nav>
   );
